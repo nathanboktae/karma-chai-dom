5eed889a72b6f3c72bf2af0507931d9311dbeddb
Nathan Black <nathan@nathanblack.org>
2015-10-06 10:09:25 -0700

    use path.join instead of concatenation

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ framework.$inject = ['config.files']
 module.exports = {
   'framework:chai-dom': ['factory', function(files) {
     files.unshift({
-      pattern: path.dirname(require.resolve('chai-dom')) + '/chai-dom.js',
+      pattern: path.join(path.dirname(require.resolve('chai-dom')), 'chai-dom.js'),
       included: true,
       served: true,
       watched: false
